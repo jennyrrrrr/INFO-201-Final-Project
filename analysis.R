1c03a214a14a1a511525d3eec44f8d85f8fff5f5
Emran <emran13@uw.edu>
2019-11-18 21:42:20 -0800

    Added new data (Undernourished) Added undernourished plot graph

--- analysis.R
+++ analysis.R
@@ -9,6 +9,18 @@ undernourished_df <- read.csv("data/prevalence-of-undernourishment.csv")
 colnames(undernourished_df)[colnames(undernourished_df)=="Prevalence.of.undernourishment....of.population.....of.population."] <- "Percent_Undernourished"
 imp_exp_df <- read.csv("data/FAOSTAT_data_11-13-2019.csv")
 
+#Global number of people who are undernourished line graph.
+undernourished.data <- read.csv("data/undernourished.csv", stringsAsFactors = FALSE)
+
+p1 <- ggplot() + 
+  geom_line(aes(y = Number.of.people.undernourished..FAO.SOFI..2018....World.Bank..2017.., x = Year), data = undernourished.data) +
+  scale_x_continuous(breaks=seq(1991,2017,2)) + scale_y_continuous(labels = scales::comma)
+theme(text=element_text(family="Tahoma"))
+
+p1 + labs(title = "Global Number of People Who Are Undernourished", x = "Year", y = "Amount of People", caption = "Source: UN FAO (2018); UN FAO (2017); World Bank (2017)")
+
+
+
 # geo styling
 l <- list(color = toRGB("grey"), width = 0.5)
 

1a15aab1cc6893414604be04ce9ca96663fb170b
Ryan Huang <ryanh7842000@gmail.com>
2019-12-03 17:46:32 -0800

    Add final graph, no UI

--- Shiny/analysis.R
+++ Shiny/analysis.R
@@ -14,6 +14,7 @@ world_undernourished_df <- read.csv("Data/undernourished.csv",
                                     stringsAsFactors = FALSE)
 colnames(world_undernourished_df)[colnames(world_undernourished_df) == "Number.of.people.undernourished..FAO.SOFI..2018....World.Bank..2017.."] <- "Number_Undernourished"
 
+regions_df <- read.csv("Data/countryContinent.csv")
 
 # ======================================================
 # Global number of people who are undernourished line graph.
@@ -110,34 +111,49 @@ exports_p <- function(){
 imp_exp_ratio <- merge(exp_df,imp_df, by = "Area")
 colnames(imp_exp_ratio)[colnames(imp_exp_ratio)=="Value.x"] <- "Export.value"
 colnames(imp_exp_ratio)[colnames(imp_exp_ratio)=="Value.y"] <- "Import.value"
+
 imp_exp_ratio$Code <- countrycode(imp_exp_ratio$Area, 'country.name', 'genc3c')
-imp_exp_ratio$Relative_Trade <- 
-  round(100 * (imp_exp_ratio$Export.value - imp_exp_ratio$Import.value) / (imp_exp_ratio$Import.value), 2)
 
-# imp_exp_ratio$Total_Transactions <- (imp_exp_ratio$Export.Value + imp_exp_ratio$Import.Value)
-
-# Calculate hunger per country (undernourishment)
-# Hunger is only from 2016 to closest match the import/export data's time of collection, 2017.
 recent_undernour <- undernourished_df %>% 
   filter(undernourished_df$Year == 2016)
 
-# Join the two dataframes for a dataframe with attributes:
-# Country, % undernourished, % importation, total import/export
-# This intermediate dataframe is messy
 joined_dataframe <- merge(imp_exp_ratio, recent_undernour, by="Code")
+
+joined_dataframe$Relative_Trade <- 
+  round((100 * joined_dataframe$Export.value / (joined_dataframe$Import.value + joined_dataframe$Export.value))
+        / joined_dataframe$Percent_Undernourished, 2)
+
+
+# Prepare regions df, only want code_3, continent, country columns
+colnames(regions_df)[colnames(regions_df)=="code_3"] <- "Code"
+colnames(regions_df)[colnames(regions_df)=="continent"] <- "Continent"
+regions_df_cleaned <- regions_df %>% 
+  select(Code, country, Continent)
+
+# Add country/continent dataframe
 primed_dataframe <- joined_dataframe %>% 
-  select(Area, Relative_Trade, Percent_Undernourished, Import.value, Export.value) %>%  #NOTE: HAVE TOTAL TRANSACTIONS IN HOVER INFO, ALONG WITH COUNTRY NAME, % RELATIVE, AND IMP/EXP NUMBERS
+  select(Code, Area, Relative_Trade, Percent_Undernourished, Import.value, Export.value) %>%  #NOTE: HAVE TOTAL TRANSACTIONS IN HOVER INFO, ALONG WITH COUNTRY NAME, % RELATIVE, AND IMP/EXP NUMBERS
   arrange(desc(Relative_Trade)) %>% 
   mutate(Area = factor(Area, Area))
 
+joined_dataframe <- merge(joined_dataframe, regions_df_cleaned, by="Code")
+joined_dataframe <- joined_dataframe %>% 
+  arrange(desc(Relative_Trade))
 
-# TODO: ADD PLOTLY ZOOM?/SIDE BY SIDE CHOOSING BY REGION
 # NOTE: TURNING scale_fill_gradien2 into scale_fill_gradient and removing "midpoint" makes the graph much
 # more colorful but less accurate.
-# TODO: Find color combo that works with midpoint/scale_fill_gradient2?
 p4 <-  
-  ggplot(primed_dataframe,aes(x=Area, y=Relative_Trade, fill=Percent_Undernourished)) +
+  ggplot(joined_dataframe,aes(x=reorder(Area, -Relative_Trade), y=Relative_Trade, fill = Continent)) +
+  theme_dark() +
+  theme(axis.text.y=element_blank(), axis.ticks.y=element_blank(),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  labs(y = "Relative Trade Transform (see analysis for transform details)",
+       x = "Country",
+       title = "Relative Trade and Percent Undernourishment by Country") +
   coord_flip() +
-  scale_fill_gradient2(low = "maroon4", high = "pink", midpoint = median(primed_dataframe$Percent_Undernourished)) +
-  geom_col()
+ # scale_fill_gradient2(low = "lightgreen", high = "red", midpoint = 20) +
+  geom_col(aes(color = Continent), color = "black") + #can do just "aes(Continent)" or can have no parameter or aes(color = Continent.)
+  theme(plot.title = element_text(hjust = 0.5))
 (p4)
+final_plot <- ggplotly(p4)
+(final_plot)
